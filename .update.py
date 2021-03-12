66bc9da1ff5ae7d569c2185520fb15c430ed3f47
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-03-12 22:41:08 +0200

    Update .update.py

--- .update.py
+++ .update.py
@@ -7,12 +7,14 @@ os.chdir(parent)
 print(os.getcwd())
 xd = input("Would you like to update? (Y/N): ")
 if xd == "Y":
-    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; mkdir Reaper/.info ; touch Reaper/.info/tos_agree.py ; python3 " + parent + "/Reaper/main.py")
+    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; mkdir Reaper/.info ; touch Reaper/.info/tos_agree.py")
     os.chdir(parent + "/Reaper")
+    os.system("python3 main.py")
     sys.exit()
 if xd == "y":
-    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; mkdir Reaper/.info ; touch Reaper/.info/tos_agree.py ; python3 " + parent + "/Reaper/main.py")
+    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; mkdir Reaper/.info ; touch Reaper/.info/tos_agree.py")
     os.chdir(parent + "/Reaper")
+    os.system("python3 main.py")
     sys.exit()
 else:
     print("ok")
