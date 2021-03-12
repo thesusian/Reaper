fd127c19ab8bccefecc9d8fb38bf6106e2f916a9
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-03-12 22:28:04 +0200

    Update .update.py

--- .update.py
+++ .update.py
@@ -3,15 +3,16 @@ import sys
 from sys import platform
 path = os.getcwd()
 parent = os.path.abspath(os.path.join(path, os.pardir))
-os.system("cd " + parent)
-os.chdir(parent*)
+os.chdir(parent)
 print(os.getcwd())
 xd = input("Would you like to update? (Y/N): ")
 if xd == "Y":
-    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; cd Reaper ; python3 main.py")
+    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; python3 main.py")
+    os.chdir(parent + "/Raper")
     sys.exit()
 if xd == "y":
-    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; cd Reaper ; python3 main.py")
+    os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; python3 main.py")
+    os.chdir(parent + "/Raper")
     sys.exit()
 else:
     print("ok")
