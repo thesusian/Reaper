e4fc332fa6127d44957e8d56f62138a7872ee217
KrisIsHere <73352269+KrisIsHere@users.noreply.github.com>
2021-03-13 15:34:42 +0200

    Update .update.py

--- tools/.update.py
+++ tools/.update.py
@@ -5,6 +5,7 @@ path = os.getcwd()
 parent = os.path.abspath(os.path.join(path, os.pardir))
 os.chdir(parent)
 print(os.getcwd())
+Print("Reaper update tool made by KrisIsHere | ver. 1.1")
 xd = input("Would you like to update? (Y/N): ")
 if xd == "Y":
     os.system("rm -rf Reaper ; git clone https://github.com/krisishere/Reaper ; cp -r .info " + parent + "/Reaper/tools/ ;")
